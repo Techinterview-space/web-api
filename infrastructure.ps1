eadefc28fbe3d02695f51c7c4d0d03947051d691
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2023-03-02 08:30:05 +0600

    Added test file

--- infrastructure.ps1
+++ infrastructure.ps1
@@ -1,3 +1,1 @@
-docker-compose -f "docker-compose.yml" stop
-docker-compose -f "docker-compose.yml" rm --force
-docker-compose -f "docker-compose.yml" up --build localstack database.api kafka zookeeper elasticsearch
+docker-compose up -d --build localstack database.api kafka zookeeper elasticsearch
