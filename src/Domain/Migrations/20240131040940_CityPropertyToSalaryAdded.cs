5d417f42a977de56195380b6cf7f3cbfcc5a3806
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-01-31 10:11:14 +0600

    Added migration

---
+++ src/Domain/Migrations/20240131040940_CityPropertyToSalaryAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CityPropertyToSalaryAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "City",
+                table: "UserSalaries",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "UserSalaries");
+        }
+    }
+}
