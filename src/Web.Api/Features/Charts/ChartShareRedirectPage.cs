b8be9e1dbc0c9ba4481da813a4c59dc862a81e4d
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-03-11 17:16:07 +0500

    Title

--- src/Web.Api/Features/Charts/ChartShareRedirectPage.cs
+++ src/Web.Api/Features/Charts/ChartShareRedirectPage.cs
@@ -35,8 +35,6 @@ public class ChartShareRedirectPageContentResultHandler
     public async Task<ContentResult> CreateAsync(
         CancellationToken cancellationToken)
     {
-        const string title = "Зарплаты в IT в Казахстане";
-
         var frontendUrl = _global.FrontendBaseUrl + "/salaries";
         string queryParams = null;
 
@@ -112,7 +110,7 @@ public class ChartShareRedirectPageContentResultHandler
     <!-- Bootstrap CSS -->
     <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"" crossorigin=""anonymous"">
 
-    <title>{title}</title>
+    <title>{ogTitle}</title>
   </head>
   <body>
     <div class=""container"">
