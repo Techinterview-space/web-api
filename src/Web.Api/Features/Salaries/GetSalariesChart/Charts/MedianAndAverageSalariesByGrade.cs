34ea36a290e4d5d2c608436b0ac00d428be27e56
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-04-08 10:02:42 +0500

    Added prop

--- src/Web.Api/Features/Salaries/GetSalariesChart/Charts/MedianAndAverageSalariesByGrade.cs
+++ src/Web.Api/Features/Salaries/GetSalariesChart/Charts/MedianAndAverageSalariesByGrade.cs
@@ -13,11 +13,16 @@ public record MedianAndAverageSalariesByGrade
         List<UserSalaryDto> salaries)
     {
         Grade = grade;
+        Count = salaries.Count;
         AverageSalary = salaries.Count > 0 ? salaries.Select(x => x.Value).Average() : null;
         MedianSalary = salaries.Count > 0 ? salaries.Select(x => x.Value).Median() : null;
     }
 
     public DeveloperGrade Grade { get; }
+
+    public int Count { get; }
+
+    public bool HasData => Count > 0;
 
     public double? AverageSalary { get; }
 
