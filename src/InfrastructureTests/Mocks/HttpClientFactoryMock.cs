bc68849e9dd57e4fe9586c8d0a7f753daea694d8
Galymzhan <mukhamedzhanov.g@abitech.kz>
2024-06-07 01:13:52 +0500

    add unit test for CurrencyService

---
+++ src/InfrastructureTests/Mocks/HttpClientFactoryMock.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+
+namespace InfrastructureTests.Mocks
+{
+    public static class HttpClientFactoryMock
+    {
+        public static Mock<IHttpClientFactory> SetupHttpClientFactoryMock(string respData)
+        {
+            var mockHandler = new Mock<HttpMessageHandler>();
+            mockHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(respData)
+                });
+
+            var httpClient = new HttpClient(mockHandler.Object);
+
+            var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+            mockHttpClientFactory
+                .Setup(factory => factory.CreateClient(It.IsAny<string>()))
+                .Returns(httpClient);
+
+            return mockHttpClientFactory;
+        }
+    }
+}
---
+++ src/InfrastructureTests/Services/CurrencyServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities.Salaries;
+using Infrastructure.Currencies;
+using InfrastructureTests.Fakes.Data;
+using InfrastructureTests.Mocks;
+using MemoryCache.Testing.Moq;
+using TestUtils.Fakes;
+using Xunit;
+
+namespace InfrastructureTests.Services
+{
+    public class CurrencyServiceTests
+    {
+        [Fact]
+        public async Task GetCurrenciesAsync__OkAsync()
+        {
+            // Arrange
+            Dictionary<string, string> configDic = new Dictionary<string, string>()
+            {
+                { "Currencies:Url", "https://currencies.com/rates_all.xml" },
+            };
+            var memoryConfig = new InMemoryConfig(configDic);
+            var mockedCache = Create.MockedMemoryCache();
+            var mockHttpClientFactory = HttpClientFactoryMock.SetupHttpClientFactoryMock(FakeXml.CurrenciesXml);
+
+            var currencyService = new CurrencyService(
+                mockHttpClientFactory.Object,
+                memoryConfig.Value(),
+                mockedCache);
+
+            // Act
+            var currencies = await currencyService.GetCurrenciesAsync();
+
+            // Assert
+            Assert.Single(currencies);
+            Assert.Equal(Currency.USD, currencies[0].Currency);
+            Assert.Equal(446.89, currencies[0].Value);
+            Assert.Equal(new DateTime(2024, 6, 7).Date, currencies[0].PubDate.Date);
+        }
+    }
+}
