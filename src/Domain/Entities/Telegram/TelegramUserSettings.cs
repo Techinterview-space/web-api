a91b861a045a26c1827d8a8475c89d5a4a1cafa7
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2024-06-21 22:19:23 +0400

    Added model

---
+++ src/Domain/Entities/Telegram/TelegramUserSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Domain.Entities.Users;
+
+namespace Domain.Entities.Telegram;
+
+public class TelegramUserSettings : HasDatesBase, IHasIdBase<Guid>
+{
+    protected TelegramUserSettings()
+    {
+    }
+
+    public TelegramUserSettings(
+        string username,
+        long chatId,
+        User user)
+    {
+        Username = username;
+        ChatId = chatId;
+        UserId = user.Id;
+        User = user;
+    }
+
+    [Required]
+    [StringLength(200)]
+    public string Username { get; protected set; }
+
+    public long ChatId { get; protected set; }
+
+    public long UserId { get; protected set; }
+
+    public virtual User User { get; protected set; }
+
+    public Guid Id { get; protected set; }
+}
---
+++ src/Infrastructure/Database/Config/TelegramUserSettingsConfig.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities.Telegram;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Database.Config;
+
+public class TelegramUserSettingsConfig : IEntityTypeConfiguration<TelegramUserSettings>
+{
+    public void Configure(
+        EntityTypeBuilder<TelegramUserSettings> builder)
+    {
+        builder.ToTable("TelegramUserSettings");
+        builder.HasKey(x => x.Id);
+
+        builder
+            .Property(x => x.Username)
+            .IsRequired(true);
+
+        builder
+            .Property(x => x.ChatId)
+            .IsRequired(true);
+
+        builder
+            .HasIndex(x => x.ChatId)
+            .IsUnique(true);
+    }
+}
