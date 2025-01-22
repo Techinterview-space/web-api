6178095b325ff00975070374f589c628a3623e01
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-01-22 16:49:22 +0500

    Changed splitter for historical data

---
+++ src/Infrastructure/Images/QRCodeImage.cs
@@ -0,0 +1,34 @@
+﻿using QRCoder;
+
+namespace Infrastructure.Images;
+
+public class QRCodeImage
+{
+    private const int DefaultPixelSize = 20;
+
+    private readonly string _value;
+
+    public QRCodeImage(
+        string value)
+    {
+        _value = value;
+    }
+
+    public byte[] AsBytes()
+    {
+        using var qrGenerator = new QRCodeGenerator();
+        using var qrCodeData = qrGenerator.CreateQrCode(_value, QRCodeGenerator.ECCLevel.Q);
+        using var qrCode = new PngByteQRCode(qrCodeData);
+
+        return qrCode.GetGraphic(DefaultPixelSize);
+    }
+
+    public string AsBase64()
+    {
+        using var qrGenerator = new QRCodeGenerator();
+        using var qrCodeData = qrGenerator.CreateQrCode(_value, QRCodeGenerator.ECCLevel.Q);
+        using var qrCode = new Base64QRCode(qrCodeData);
+
+        return qrCode.GetGraphic(DefaultPixelSize);
+    }
+}
