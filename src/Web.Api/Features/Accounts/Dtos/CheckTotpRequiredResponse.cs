5c098e96c29a44495627fe006e4d8afa701a3de5
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-01-28 09:09:03 +0500

    Added check totp endpoint

---
+++ src/Web.Api/Features/Accounts/Dtos/CheckTotpRequiredResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.Api.Features.Accounts.Dtos;
+
+public record CheckTotpRequiredResponse
+{
+    public long Id { get; init; }
+
+    public string Email { get; init; }
+
+    public bool IsMfaEnabled { get; init; }
+}
