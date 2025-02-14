555cf503429048bf70b9f5ec28fd271e0935586a
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-02-14 15:17:57 +0500

    Migration added

---
+++ src/Infrastructure/Migrations/20250214101618_UsefulnessRatingPropertyAdded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsefulnessRatingPropertyAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectationReply",
+                table: "SalariesSurveyReplies");
+
+            migrationBuilder.RenameColumn(
+                name: "UsefulnessReply",
+                table: "SalariesSurveyReplies",
+                newName: "UsefulnessRating");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UsefulnessRating",
+                table: "SalariesSurveyReplies",
+                newName: "UsefulnessReply");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ExpectationReply",
+                table: "SalariesSurveyReplies",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
