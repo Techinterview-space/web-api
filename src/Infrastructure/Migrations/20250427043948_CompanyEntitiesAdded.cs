83a6c37d9ebac486c181db5ed42f77ece0d29c37
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-04-27 09:40:07 +0500

    Migration added

---
+++ src/Infrastructure/Migrations/20250427043948_CompanyEntitiesAdded.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyEntitiesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    NormalizedName = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Links = table.Column<string>(type: "text", nullable: true),
+                    LogoUrl = table.Column<string>(type: "text", nullable: true),
+                    Rating = table.Column<double>(type: "double precision", nullable: false),
+                    ReviewsCount = table.Column<int>(type: "integer", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyRatingHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Rating = table.Column<double>(type: "double precision", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyRatingHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompanyRatingHistory_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyReviews",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CultureAndValues = table.Column<int>(type: "integer", nullable: false),
+                    CodeQuality = table.Column<int>(type: "integer", nullable: true),
+                    WorkLifeBalance = table.Column<int>(type: "integer", nullable: false),
+                    Management = table.Column<int>(type: "integer", nullable: false),
+                    CompensationAndBenefits = table.Column<int>(type: "integer", nullable: false),
+                    CareerOpportunities = table.Column<int>(type: "integer", nullable: false),
+                    TotalRating = table.Column<double>(type: "double precision", nullable: false),
+                    Pros = table.Column<string>(type: "text", nullable: true),
+                    Cons = table.Column<string>(type: "text", nullable: true),
+                    IWorkHere = table.Column<bool>(type: "boolean", nullable: false),
+                    UserEmployment = table.Column<int>(type: "integer", nullable: false),
+                    ApprovedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    OutdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CompanyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CompanyReviews_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyReviews_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_NormalizedName",
+                table: "Companies",
+                column: "NormalizedName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyRatingHistory_CompanyId",
+                table: "CompanyRatingHistory",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyReviews_CompanyId",
+                table: "CompanyReviews",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyReviews_UserId",
+                table: "CompanyReviews",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyRatingHistory");
+
+            migrationBuilder.DropTable(
+                name: "CompanyReviews");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
