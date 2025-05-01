def30b71777c36279aab2a768f5bf7ca1db9748f
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-05-01 18:22:33 +0500

    Added jwt generator

---
+++ src/Infrastructure/Jwt/JwtTokenGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Infrastructure.Jwt;
+
+public record JwtTokenGenerator
+{
+    public const string DefaultIssuer = "techinterview-space";
+    public const string DefaultAudience = "techinterview-space-bot";
+
+    private readonly string _secretKey;
+    private readonly string _issuer;
+    private readonly string _audience;
+    private readonly int _expirationMinutes;
+
+    private string _result;
+
+    public JwtTokenGenerator(
+        string secretKey,
+        string issuer = DefaultIssuer,
+        string audience = DefaultAudience,
+        int expirationMinutes = 60)
+    {
+        _secretKey = secretKey;
+        _issuer = issuer;
+        _audience = audience;
+        _expirationMinutes = expirationMinutes;
+    }
+
+    public override string ToString()
+    {
+        return _result ??= GenerateInternal();
+    }
+
+    private string GenerateInternal()
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new List<Claim>();
+
+        var token = new JwtSecurityToken(
+            issuer: _issuer,
+            audience: _audience,
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(_expirationMinutes),
+            signingCredentials: credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
---
+++ src/InfrastructureTests/Jwt/JwtTokenGeneratorTests.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Jwt;
+using Xunit;
+
+namespace InfrastructureTests.Jwt;
+
+public class JwtTokenGeneratorTests
+{
+    [Fact]
+    public void ToString_ShouldReturnToken()
+    {
+        // Arrange
+        var secretKey = "my-secret-phrase-please-do-not-use-this-in-production";
+        var generator = new JwtTokenGenerator(secretKey);
+
+        var result = generator.ToString();
+        Assert.Contains("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9", result);
+    }
+}
