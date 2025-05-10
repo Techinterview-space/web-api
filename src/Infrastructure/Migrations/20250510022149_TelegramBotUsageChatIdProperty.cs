31dc1950066483f5f6d71644c6df20ffa084b91e
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-05-10 07:22:16 +0500

    Migration

---
+++ src/Infrastructure/Migrations/20250510022149_TelegramBotUsageChatIdProperty.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class TelegramBotUsageChatIdProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ChatId",
+                table: "TelegramBotUsages",
+                type: "bigint",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChatId",
+                table: "TelegramBotUsages");
+        }
+    }
+}
