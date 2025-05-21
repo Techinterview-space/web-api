fb568342b6406b05f9a40535308f7a77bb6324a2
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-05-21 18:07:52 +0500

    Migration

---
+++ src/Infrastructure/Migrations/20250521130714_SubscriptionRegularityPropAdded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubscriptionRegularityPropAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Regularity",
+                table: "StatDataChangeSubscriptions",
+                type: "text",
+                nullable: false,
+                defaultValue: "Weekly");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Regularity",
+                table: "StatDataChangeSubscriptions");
+        }
+    }
+}
