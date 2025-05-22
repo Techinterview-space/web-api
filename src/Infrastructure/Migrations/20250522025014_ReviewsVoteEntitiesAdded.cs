775b8c98fc58b075e13981a38a9742da632e7a04
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-05-22 07:50:53 +0500

    Added migration

---
+++ src/Infrastructure/Migrations/20250522025014_ReviewsVoteEntitiesAdded.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReviewsVoteEntitiesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DislikesCount",
+                table: "CompanyReviews",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LikesCount",
+                table: "CompanyReviews",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CompanyReviewVotes",
+                columns: table => new
+                {
+                    ReviewId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: false),
+                    VoteType = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyReviewVotes", x => new { x.ReviewId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_CompanyReviewVotes_CompanyReviews_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "CompanyReviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CompanyReviewVotes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyReviewVotes_UserId",
+                table: "CompanyReviewVotes",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyReviewVotes");
+
+            migrationBuilder.DropColumn(
+                name: "DislikesCount",
+                table: "CompanyReviews");
+
+            migrationBuilder.DropColumn(
+                name: "LikesCount",
+                table: "CompanyReviews");
+        }
+    }
+}
