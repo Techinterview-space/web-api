3f4ad66da5139597c58ffc1cd46feeaa20b8a82c
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-05-28 17:43:49 +0500

    added email preview service

---
+++ src/Web.Api/Features/Emails/EmailPreviewController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Domain.Enums;
+using Infrastructure.Emails;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Features.Emails.ViewModels;
+using Web.Api.Setup.Attributes;
+
+namespace Web.Api.Features.Emails;
+
+[ApiController]
+[Route("api/admin/email-previews")]
+[HasAnyRole(Role.Admin)]
+public class EmailPreviewController : ControllerBase
+{
+    private readonly IViewRenderer _viewRenderer;
+
+    public EmailPreviewController(
+        IViewRenderer viewRenderer)
+    {
+        _viewRenderer = viewRenderer;
+    }
+
+    [HttpGet("review-approved")]
+    public async Task<EmailPreviewResponse> ReviewWasApproved()
+    {
+        var view = await _viewRenderer.RenderHtmlAsync(
+            ReviewWasApprovedViewModel.ViewName,
+            new ReviewWasApprovedViewModel("Company Name"));
+
+        return new EmailPreviewResponse(view);
+    }
+}
