f7c93e2eff42445b6df76ec7425e949a6984e63a
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-06-01 06:57:13 +0500

    fixed title

--- src/Web.Api/Features/Emails/ViewModels/SalaryUpdateReminderViewModel.cs
+++ src/Web.Api/Features/Emails/ViewModels/SalaryUpdateReminderViewModel.cs
@@ -4,7 +4,7 @@ public record SalaryUpdateReminderViewModel : ViewModelBase
 {
     public const string ViewName = "/Views/EmailTemplates/SalaryUpdateReminder.cshtml";
 
-    public const string Subject = "Обновим вашу зарплату?";
+    public const string Subject = "Techinterview - вы давно оставили анкету о зарплате";
 
     public SalaryUpdateReminderViewModel(
         string unsubscribeToken)
