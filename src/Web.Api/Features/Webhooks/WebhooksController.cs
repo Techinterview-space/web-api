a005e75276c243c1be17565bf14cc31722b77e6c
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-06-12 08:55:13 +0500

    Added webhook for sendgrid

---
+++ src/Web.Api/Features/Webhooks/WebhooksController.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Api.Features.Webhooks;
+
+[ApiController]
+[Route("api/webhooks")]
+public class WebhooksController : ControllerBase
+{
+    private readonly ILogger<WebhooksController> _logger;
+
+    public WebhooksController(
+        ILogger<WebhooksController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost("sendgrid")]
+    public IActionResult SendgridWebhook()
+    {
+        var bodyStream = new StreamReader(Request.Body);
+        bodyStream.BaseStream.Seek(0, SeekOrigin.Begin);
+        var bodyText = bodyStream.ReadToEnd();
+
+        _logger.LogInformation(
+            "Sendgrid webhook. Body: {Body}",
+            bodyText);
+
+        return Ok();
+    }
+}
