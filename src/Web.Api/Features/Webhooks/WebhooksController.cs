02ec5fcc3030da54432f198f9ba5c718807852ed
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-06-12 09:01:10 +0500

    adjusted

--- src/Web.Api/Features/Webhooks/WebhooksController.cs
+++ src/Web.Api/Features/Webhooks/WebhooksController.cs
@@ -1,4 +1,7 @@
 ﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -17,17 +20,23 @@ public class WebhooksController : ControllerBase
     }
 
     [HttpPost("sendgrid")]
-    public IActionResult SendgridWebhook()
+    public async Task<IActionResult> SendgridWebhook()
     {
-        var bodyStream = new StreamReader(Request.Body);
-        bodyStream.BaseStream.Seek(0, SeekOrigin.Begin);
-        var bodyText = bodyStream.ReadToEnd();
+        // Enable seeking on the stream
+        Request.EnableBuffering();
+
+        // Read the body as a string
+        using var reader = new StreamReader(Request.Body, Encoding.UTF8, leaveOpen: true);
+        var rawBody = await reader.ReadToEndAsync();
+
+        // Reset the stream position for further processing if needed
+        Request.Body.Position = 0;
 
         var signature = Request.Headers["X-Twilio-Email-Event-Webhook-Signature"].ToString();
 
         _logger.LogInformation(
             "Sendgrid webhook. Body: {Body}. Signature: {Signature}",
-            bodyText,
+            rawBody,
             signature);
 
         return Ok();
