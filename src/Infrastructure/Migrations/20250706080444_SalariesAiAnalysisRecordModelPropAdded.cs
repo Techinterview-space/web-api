cf0359a3be5d842f7263a2e99c211ce9652c3948
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-07-06 13:09:00 +0500

    Migration added

---
+++ src/Infrastructure/Migrations/20250706080444_SalariesAiAnalysisRecordModelPropAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class SalariesAiAnalysisRecordModelPropAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Model",
+                table: "AiAnalysisRecords",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Model",
+                table: "AiAnalysisRecords");
+        }
+    }
+}
