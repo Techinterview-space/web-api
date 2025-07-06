4aade0c7d725c955e1f8f857ddf9df6da0afffb4
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2025-07-06 17:50:07 +0500

    Added class

---
+++ src/Infrastructure/Services/Html/MarkdownToTelegramHtml.cs
@@ -0,0 +1,19 @@
+﻿namespace Infrastructure.Services.Html;
+
+public class MarkdownToTelegramHtml
+{
+    public string Source { get; }
+
+    public string Html { get; }
+
+    public MarkdownToTelegramHtml(
+        string source)
+    {
+        Source = source;
+    }
+
+    public override string ToString()
+    {
+        return Html;
+    }
+}
