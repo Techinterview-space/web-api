e8289b3bdd79db557a959946e8f6867e55530c57
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-01-02 15:26:27 +0500

    Added migration

---
+++ src/Infrastructure/Migrations/20260102102556_CurrencyCollectionEntityAdded.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CurrencyCollectionEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrenciesCollections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Currencies = table.Column<string>(type: "text", nullable: true),
+                    CurrencyDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrenciesCollections", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CurrenciesCollections_CurrencyDate",
+                table: "CurrenciesCollections",
+                column: "CurrencyDate",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrenciesCollections");
+        }
+    }
+}
