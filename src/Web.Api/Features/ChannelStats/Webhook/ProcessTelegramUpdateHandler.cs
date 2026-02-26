a7067faf1ae6cea3f83ec4b6277e39d4dc7f7188
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-02-26 22:23:54 +0500

    +

--- src/Web.Api/Features/ChannelStats/Webhook/ProcessTelegramUpdateHandler.cs
+++ src/Web.Api/Features/ChannelStats/Webhook/ProcessTelegramUpdateHandler.cs
@@ -43,6 +43,10 @@ public class ProcessTelegramUpdateHandler
             return Nothing.Value;
         }
 
+        _logger.LogInformation(
+            "Telegram webhook body: {UpdateJson}",
+            request.PayloadJson);
+
         var rawUpdate = new TelegramRawUpdate(request.UpdateId, request.PayloadJson);
         await _context.SaveAsync(rawUpdate, cancellationToken);
 
