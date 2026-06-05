d23309c965e8c70f7cb697363c82c9d7870b72d5
maxim.gorbatyuk <maxim.gorbatyuk@outlook.com>
2026-06-05 09:51:06 +0500

    Migration added

---
+++ src/Infrastructure/Migrations/20260605045049_VacancyEntityAdded.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class VacancyEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vacancies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    CompanyId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CompanyName = table.Column<string>(type: "text", nullable: true),
+                    HideAttachedCompany = table.Column<bool>(type: "boolean", nullable: false),
+                    AuthorId = table.Column<long>(type: "bigint", nullable: false),
+                    HrContact = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacancies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Vacancies_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Vacancies_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VacancyHistory",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    VacancyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false),
+                    CreatedBy = table.Column<string>(type: "character varying(320)", maxLength: 320, nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VacancyHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VacancyHistory_Vacancies_VacancyId",
+                        column: x => x.VacancyId,
+                        principalTable: "Vacancies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_AuthorId",
+                table: "Vacancies",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_CompanyId",
+                table: "Vacancies",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_CreatedAt",
+                table: "Vacancies",
+                column: "CreatedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vacancies_Status",
+                table: "Vacancies",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VacancyHistory_VacancyId",
+                table: "VacancyHistory",
+                column: "VacancyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VacancyHistory");
+
+            migrationBuilder.DropTable(
+                name: "Vacancies");
+        }
+    }
+}
